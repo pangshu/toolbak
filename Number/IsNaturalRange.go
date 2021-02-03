6450a06d5759dae848031cce55e795db2d03a493
pangshu <ipqqqug@qq.com>
2021-02-03 22:16:47 +0800

    修复bug

--- Number/IsNaturalRange.go
+++ Number/IsNaturalRange.go
@@ -1,10 +1,13 @@
 package Number
 
-import "toolbak/Array"
+import (
+	"toolbak/Array"
+)
 
 // IsNaturalRange 是否连续的自然数数组/切片,如[0,1,2,3...],其中不能有间断.
 // strict为是否严格检查元素的顺序.
 func (toolNumber *Number)IsNaturalRange(arr []int, strict bool) (res bool) {
+	var toolArray Array.Array
 	n := len(arr)
 	if n == 0 {
 		return
@@ -17,7 +20,7 @@ func (toolNumber *Number)IsNaturalRange(arr []int, strict bool) (res bool) {
 		ctyp = "ALL"
 	}
 
-	diff := Array.ArrayDiff(orig, arr, ctyp)
+	diff := toolArray.ArrayDiff(orig, arr, ctyp)
 
 	res = len(diff) == 0
 	return
